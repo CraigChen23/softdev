3ac85eec78dfbe5b2fa0795951086c7ee1ad6752
Craig Chen <cchen33@stuy.edu>
2023-04-04 09:28:41 -0400

    work

---
+++ 27_js/fibfac.js
@@ -0,0 +1,15 @@
+// Team Expensive :: Craig Chen, Faiyaz Rafee
+// SoftDev pd2
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// implement a fact and fib fxn
+
+(define fact(x(n)
+(if(< n 2)
+    1
+(*n(fact(- n 1)))
+
+))
